f23aa3ff577764028f7c65191e375a07cb8e8dfe
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2023-08-06 19:37:16 +0800

    Add home page

---
+++ SPringBoard/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import MDashboard from '../components/Dashboard/mentor_dashboard/MDashboard';
+const Home = () => {
+  return (
+    <MDashboard />
+  )
+}
+
+export default Home;
