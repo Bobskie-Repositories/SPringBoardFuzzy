73b7a4d09211713361da872b9a676207b87b97c2
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2023-08-08 11:40:53 +0800

    Update mentor dashboard

--- frontend/src/components/Dashboard/mentor_dashboard/MDashboard.jsx
+++ frontend/src/components/Dashboard/mentor_dashboard/MDashboard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Sidebar from '../../Sidebar/Sidebar';
+import Search from '../../Search/Search';
+import Profile from '../../ProfileSegment/Profile';
+import Classroom from '../../Classroom/Classroom';
+import styles from './MDashboard.module.css';
 
 const MDashboard = () => {
   return (
-    <Sidebar />
+    <div className={ styles.container }>
+      <Sidebar />
+
+      <div>
+        <div className={ styles.container }style={{padding: 0, gap: '200px'}}>
+          <Search />
+          <Profile identification={1} />
+        </div>
+        <Classroom />
+
+      </div>  
+
+    </div>
   )
 }
 
