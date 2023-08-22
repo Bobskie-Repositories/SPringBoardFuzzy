076a76a3c977c9817aa5d2f3d81ede8889c75811
Charlette Tulod <charlette.tulod@cit.edu>
2023-08-22 22:15:37 +0800

    add dropdown for board

--- frontend/src/components/SidebarSegment/SidebarSegment.jsx
+++ frontend/src/components/SidebarSegment/SidebarSegment.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import styles from './SidebarSegment.module.css';
+import global from '../../assets/global.module.css';
 import GroupIcon from '@assets/groupicon.png';
-
+import BoardIcon from '@assets/boardicon.png';
+import DropDownIcon from '@assets/dropdownicon.png';
 
 function SidebarSegment() {
   return (
     <div className={styles.body}>
+
+      <div className={styles.subbody}>
       <h4 className={styles.grouptext}>Your Group</h4>
       <button className={styles.groupbutton}>
           <div>
@@ -18,6 +22,15 @@ function SidebarSegment() {
             <h4>ABC-QWE-123</h4>
           </div>
       </button>
+      </div>
+
+      <ol style={{ listStyleType: "none", padding: 0, marginTop: "5%" }}>
+            <li className={`${global.center} ${styles.customLi}`} >
+                <img src={DropDownIcon} alt="DropDownIcon" /> &nbsp;
+                <img src={BoardIcon} alt="BoardIcon" className={styles.boardpic}/> &nbsp;
+                Board
+            </li>         
+        </ol>
     </div>
   )
 }
