3d29cccee42599334ca68bcbc5d39afe98669f9f
Charlette Tulod <charlette.tulod@cit.edu>
2023-08-28 23:05:23 +0800

    add useState to hide and unhide

--- frontend/src/components/SidebarSegment/SidebarSegment.jsx
+++ frontend/src/components/SidebarSegment/SidebarSegment.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SidebarSegment.module.css';
 import global from '../../assets/global.module.css';
 import GroupIcon from '@assets/groupicon.png';
@@ -7,6 +7,7 @@ import DropDownIcon from '@assets/dropdownicon.png';
 
 function SidebarSegment() {
   const Menus = ["Project Name 1", "Project Name 2"];
+  const [open, setOpen] = useState(false);
   return (
     <div className={styles.body}>
 
@@ -26,17 +27,21 @@ function SidebarSegment() {
       </div>
 
       <ol style={{ listStyleType: "none", padding: 0, marginTop: "5%" }}>
-          <li className={`${global.center} ${styles.customLi}`} >
+          <li className={`${global.center} ${styles.customLi}`} onClick={() => setOpen(!open)}>
             <img src={DropDownIcon} alt="DropDownIcon" /> &nbsp;
             <img src={BoardIcon} alt="BoardIcon" className={styles.boardpic}/> &nbsp;
             Board
           </li>         
       </ol>
-      <ul style={{  marginTop: "-7%", paddingLeft: "30%"}} >
+
+      { open && (
+          <ul style={{  marginTop: "-7%", paddingLeft: "30%"}} >
         {Menus.map((menu) => (
           <li className={styles.projectName} key={menu} >{menu}</li>
         ))}
       </ul>
+      )}
+      
     </div>
   )
 }
