34197c265007539aa422c2389570a4846149f33a
Charlette Tulod <charlette.tulod@cit.edu>
2023-09-03 23:09:43 +0800

    Add board component to student dashboard

---
+++ frontend/src/components/Boards/Board.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Board.module.css';
+import Card from '../UI/Card/Card';
+
+function Board() {
+  return (
+    <div>
+        <h2 style={{fontSize: "30px", color: '#9c7b16'}}>Boards</h2>
+
+        <Card className={ styles.card }>
+
+          
+
+            
+      </Card>
+    </div>
+  )
+}
+
+export default Board
