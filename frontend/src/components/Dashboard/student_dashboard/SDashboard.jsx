430efb8394fe1e2b4e5fbd5594ea2abec7738bc1
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2023-09-27 02:55:19 +0800

    Update SDashBoard

--- frontend/src/components/Dashboard/student_dashboard/SDashboard.jsx
+++ frontend/src/components/Dashboard/student_dashboard/SDashboard.jsx
@@ -1,24 +1,48 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import Sidebar from '../../Sidebar/Sidebar';
 import Search from '../../Search/Search';
 import Boards from '../../Boards/Board';
 import Profile from '../../ProfileSegment/Profile';
+import BoardCreation from '../../BoardCreation/BoardCreation';
+import Button from '../../UI/Button/Button';
 import styles from './SDashboard.module.css';
-import { useState, useEffect } from 'react';
 
 const SDashboard = () => {
   const [selectedProject, setSelectedProject] = useState();
+  const [createAction, setCreateAction] = useState(false);
+
+  const handleCreateBoardClick = () => {
+    setCreateAction(!createAction);
+  };
 
   return (
     <div className={ styles.container } style={{padding: '20px 150px 20px 30px'}}>
       <Sidebar setSelectedProject={setSelectedProject}/>
 
       <div>
-        <div className={ styles.container }>
+        <div className={ styles.container } style={{gap: "150px"}}>
           <Search />
           <Profile identification={1} />
         </div>
-        <Boards selectedProject={selectedProject}/>
+
+        <div className={ styles.container }>
+
+          {createAction ? <BoardCreation /> : <Boards selectedProject={selectedProject}/>}
+        
+          <Button 
+            style={{
+              backgroundColor: '#9C7B16',
+              height: '40px',
+              marginTop: '40px',
+              fontSize: '12px',
+            }}
+            onClick={handleCreateBoardClick} 
+            > 
+              Create Board
+          </Button>
+        </div>
+        
       </div>  
 
     </div>
