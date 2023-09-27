875db1a60264e7796f247dbe2c68c56180791e34
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2023-09-27 22:12:26 +0800

    Update Profile

--- frontend/src/components/ProfileSegment/Profile.jsx
+++ frontend/src/components/ProfileSegment/Profile.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import prof from "../../assets/iconprof.png";
+import profwhite from '@assets/iconprof2.png';
 import styles from "./Profile.module.css"
 
 const Profile = ( {name, img, identification} ) => {
   return (
-    <div className={ styles.center } style={{height: "20px" , marginTop: '30px', gap: "20px"}}>
-        { identification == 1 ? 'Bob Kyle L. Rosales' : ''}
-        <img src={prof} alt="prof"/>
-        { identification != 1 ? 'Bob Kyle L. Rosales' : ''}
+    <div className={ styles.center } style={{height: "20px" ,  gap: "20px"}}>
+        {identification === 1 ? (
+          <div style={{marginTop: '30px',}}>
+            <p>Bob Kyle L. Rosales</p>
+            <img src={prof} alt="prof" />
+          </div>
+        ) : (
+          <>
+            <img src={profwhite} alt="profwhite" />
+            <p style={{color: 'white'}}>Bob Kyle L. Rosales</p>
+          </>
+        )}
     </div>
   )
 }
