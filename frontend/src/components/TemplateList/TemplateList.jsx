883026af99a6e2a776d2f5e7d2f621782c86f7d9
Charlette Tulod <charlette.tulod@cit.edu>
2023-10-15 19:45:55 +0800

    Add initial design

--- frontend/src/components/TemplateList/TemplateList.jsx
+++ frontend/src/components/TemplateList/TemplateList.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import styles from './TemplateList.module.css';
+import IdeaIcon from '@assets/idea.png';
+import Card from '../UI/Card/Card';
 
 const TemplateList = () => {
   return (
-    <div>TemplateList</div>
+    <div  style={{padding: '20px 150px 20px 30px'}}>
+
+      <div className={ styles.container }>   
+                <Card className={styles.container_card} >
+                  <div className={styles.scrollable}>
+                 
+                      <Card className={styles.container_board} >
+                          <div>
+                            <img 
+                                className={styles.ideaicon} 
+                                src={IdeaIcon} 
+                                alt="IdeaIcon" 
+                            />
+                          </div>
+
+                        <div className={styles.words}>
+                          <h4>Idea Venture</h4>
+                          <p>
+                           Template Description
+                          </p>
+                        </div> 
+                      </Card>
+                  </div>
+                </Card>
+      </div>
+      
+  </div>
   )
 }
 
