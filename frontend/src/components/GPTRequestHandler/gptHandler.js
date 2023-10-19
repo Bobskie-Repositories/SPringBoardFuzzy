d20f92861a8d6d7c059a55abb4d704501cddba82
chlarence <chlarence.callelero@cit.edu>
2023-10-19 23:25:27 +0800

    try gpt request

---
+++ frontend/src/components/GPTRequestHandler/gptHandler.js
@@ -0,0 +1,25 @@
+const apiKey = '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';
+const organizationId = 'CdWsXeXSpiYq3JpfKktzNc7w';
+
+export async function generateResponse(prompt) {
+  const apiUrl = 'https://api.openai.com/v1/chat/completions';
+
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: prompt }],
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data from GPT API: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.choices[0].message.content;
+}
