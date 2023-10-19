3cb4740de1cf063f2769bdd6b8e114b291a1e08c
Kath Munar <kcbodomo1526@gmail.com>
2023-10-20 00:01:21 +0800

    Remove dummy text

--- frontend/src/components/ResultBoard/ResultBoard.jsx
+++ frontend/src/components/ResultBoard/ResultBoard.jsx
@@ -67,19 +67,19 @@ const ResultBoard = ({ boardid }) => {
           <div className={styles.advice}>
             <h4>Feedback</h4>
             <div className={styles.content}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
+              {/* Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              type and scrambled it to make a type specimen book. */}
             </div>
           </div>
           <div className={styles.advice}>
             <h4>Recommendations</h4>
             <div className={styles.content}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
+              {/* Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              type and scrambled it to make a type specimen book. */}
             </div>
           </div>
         </div>
