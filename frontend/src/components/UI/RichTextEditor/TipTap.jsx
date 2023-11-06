30911980f66395484ac512ccec98560fc0c3b3c7
Charlette Tulod <charlette.tulod@cit.edu>
2023-11-06 20:54:56 +0800

    add temporary react tiptap

---
+++ frontend/src/components/UI/RichTextEditor/TipTap.jsx
@@ -0,0 +1,153 @@
+import { useEditor, EditorContent } from "@tiptap/react";
+import styles from "./Tiptap.module.css";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+import Table from "@tiptap/extension-table";
+import TableRow from "@tiptap/extension-table-row";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import Image from "@tiptap/extension-image";
+import {
+  FaBold,
+  FaHeading,
+  FaItalic,
+  FaListOl,
+  FaListUl,
+  FaQuoteLeft,
+  FaRedo,
+  FaStrikethrough,
+  FaUnderline,
+  FaUndo,
+  FaTable, // Import a suitable table icon
+  FaImage, // Import an image icon
+} from "react-icons/fa";
+
+const MenuBar = ({ editor }) => {
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div className={styles.menuBar}>
+      <div>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive("bold") ? "is_active" : ""}
+        >
+          <FaBold />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive("italic") ? "is_active" : ""}
+        >
+          <FaItalic />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+          className={editor.isActive("underline") ? "is_active" : ""}
+        >
+          <FaUnderline />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={editor.isActive("strike") ? "is_active" : ""}
+        >
+          <FaStrikethrough />
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 2 }) ? "is_active" : ""
+          }
+        >
+          <FaHeading />
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 3 }) ? "is_active" : ""
+          }
+        >
+          <FaHeading className="heading3" />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") ? "is_active" : ""}
+        >
+          <FaListUl />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("orderedList") ? "is_active" : ""}
+        >
+          <FaListOl />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive("blockquote") ? "is_active" : ""}
+        >
+          <FaQuoteLeft />
+        </button>
+
+        <button
+          onClick={() => {
+            editor.chain().focus().insertTable({ rows: 3, cols: 3 }).run();
+          }}
+        >
+          <FaTable />
+        </button>
+
+        <button
+          onClick={() => {
+            const imageUrl = prompt("Enter the image URL:");
+            if (imageUrl) {
+              editor.chain().focus().setImage({ src: imageUrl }).run();
+            }
+          }}
+        >
+          <FaImage />
+        </button>
+      </div>
+      <div>
+        <button onClick={() => editor.chain().focus().undo().run()}>
+          <FaUndo />
+        </button>
+        <button onClick={() => editor.chain().focus().redo().run()}>
+          <FaRedo />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const Tiptap = ({ setDescription }) => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+    ],
+    content: ``,
+
+    onUpdate: ({ editor }) => {
+      const html = editor.getHTML();
+      setDescription(html);
+    },
+  });
+
+  return (
+    <div className={styles.textEditor}>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
+};
