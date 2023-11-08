c6c8135e1e4f917639d1356a6b774aac3e59ff2a
chlarence <chlarence.callelero@cit.edu>
2023-11-08 21:43:30 +0800

    refine ApiKey prompt

--- frontend/src/components/GPTRequestHandler/GPTChat.jsx
+++ frontend/src/components/GPTRequestHandler/GPTChat.jsx
@@ -1,28 +1,62 @@
-import React, { useState } from 'react';
-import { generateResponse } from './gptHandler';
-//import { generateResponse } from "./components/GPTRequestHandler/gptHandler.js";
+const apiKey = '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';
+const organizationId = 'CdWsXeXSpiYq3JpfKktzNc7w';
 
-function GPTChat() {
-  const [input, setInput] = useState('');
-  const [response, setResponse] = useState('');
+export async function generateResponse(prompt) {
+  const apiUrl = 'https://api.openai.com/v1/chat/completions';
 
-  const handleGenerateResponse = async () => {
-    try {
-      const generatedResponse = await generateResponse(input);
-      setResponse(generatedResponse);
-    } catch (error) {
-      console.error('Error generating response:', error);
-    }
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: prompt }],
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data from GPT API: ${response.status}`);
   }
 
-  return ( 
-    <div>
-      <h2>Chat with GPT</h2>
-      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={handleGenerateResponse}>Generate Response</button>
-      <p>{response}</p>
-    </div>
-  );
+  const data = await response.json();
+  return data.choices[0].message.content;
 }
 
-export default GPTChat;
+// New function
+export async function RequestHandler(prompt, httpClientFactory) {
+  const _httpClientFactory = httpClientFactory;
+
+  const requestPayload = {
+    prompt: prompt,
+    temperature: 0.5,
+    max_tokens: 256,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0
+  };
+
+  const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
+
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 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', // Replace with your API key
+    },
+    body: JSON.stringify(requestPayload),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data from GPT API: ${response.status}`);
+  }
+
+  const responseData = await response.json();
+
+  if (responseData.choices.length > 0) {
+    return responseData.choices[0].text.trim();
+  } else {
+    return 'No response content or choices found.';
+  }
+}
