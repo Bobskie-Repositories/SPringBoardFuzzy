f8eb298b33827934bc17534495394988caeeb576
Charlette Tulod <charlette.tulod@cit.edu>
2023-12-03 23:32:27 +0800

    add components in landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Landing from "../components/Landing/Landing";
+import Navigation from "../components/NavigationBar/NavBar";
+import ScrollSnapContainer from "../components/UI/ScrollSnap/ScrollSnapContainer";
 
 const LandingPage = () => {
   return (
     <div>
+      <Navigation />
       <Landing />
     </div>
   );
