09f15939bf596153b38595e5a723b42d7106614d
Charlette Tulod <charlette.tulod@cit.edu>
2023-12-05 00:36:33 +0800

    add minor fix

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Landing from "../components/Landing/Landing";
 import Navigation from "../components/NavigationBar/NavBar";
-import ScrollSnapContainer from "../components/UI/ScrollSnap/ScrollSnapContainer";
 
 const LandingPage = () => {
   return (
