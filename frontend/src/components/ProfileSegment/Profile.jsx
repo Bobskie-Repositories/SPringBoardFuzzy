e2ba7afd222374dc82184a62e2f108fd3f3094e3
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-01-14 22:38:21 +0800

    Add dropdown and logout

--- frontend/src/components/ProfileSegment/Profile.jsx
+++ frontend/src/components/ProfileSegment/Profile.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import prof from "../../assets/iconprof.png";
 import profwhite from "@assets/iconprof2.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Profile.module.css";
 import { useNavigate } from "react-router";
 import { useAuth } from "../../context/AuthContext";
@@ -8,30 +10,80 @@ import { useAuth } from "../../context/AuthContext";
 const Profile = ({ identification }) => {
   const navigate = useNavigate();
   const [name, setName] = useState();
-  const { getUser } = useAuth();
+  const { getUser, logout } = useAuth();
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const user = await getUser();
       setName(user.firstname + " " + user.lastname);
     };
-
     fetchData();
   }, []);
 
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleDropdownClick = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setDropdownVisible(false);
+    }
+  };
+
+  const goLogout = async () => {
+    await logout();
+    navigate(`/login`);
+  };
+
   return (
     <div className={styles.center} style={{ height: "20px", gap: "20px" }}>
-      {identification === 1 ? (
-        <>
-          <p>{name}</p>
-          <img src={prof} alt="prof" />
-        </>
-      ) : (
-        <>
-          <img src={profwhite} alt="profwhite" />
-          <p style={{ color: "white" }}>{name}</p>
-        </>
-      )}
+      <div className={styles.dropdown} ref={dropdownRef}>
+        {identification === 1 ? (
+          <>
+            <p>{name}</p>
+            <div
+              className={`${styles.dropbtn} ${styles.img}`}
+              style={{ marginLeft: "10px" }}
+              onClick={handleDropdownClick}
+            >
+              <img src={prof} alt="prof" />
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              className={`${styles.dropbtn} ${styles.img}`}
+              style={{ marginRight: "10px" }}
+              onClick={handleDropdownClick}
+            >
+              <img src={profwhite} alt="profwhite" />
+            </div>
+            <p style={{ color: "white" }}>{name}</p>
+          </>
+        )}
+
+        {dropdownVisible && (
+          <div className={styles.dropdowncontent}>
+            <span onClick={goLogout}>
+              <FontAwesomeIcon
+                icon={faArrowRightFromBracket}
+                size="lg"
+                className={styles.icon}
+              />
+              Logout
+            </span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
