8cb262ace70cc9a9ba9b6338383aec02af74e850
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-02-02 21:11:14 +0800

    update controllers and settings

--- backend/springboard_api/urls.py
+++ backend/springboard_api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .controllers.ClassroomController import CreateClassroom, GetAllClassrooms, GetClassroomGroupsAndProjects, GetClassroom, GetClassroomById,  GetTopProjectsByClassroom
-from .controllers.ProjectController import ProjectCreateView, ProjectView, GetProjectsByGroupId, GetActiveProjectsView, GetProjectById, GetPublicProjectsByGroupId, InactiveProjectsView, ProjectCreateView, ProjectUpdateView, UpdateProjectStatusView, UpdateProjectScoreView, DeleteProjectView
+from .controllers.ProjectController import ProjectCreateView, ProjectView, GetGroupsAndProjects, GetProjectsByGroupId, GetActiveProjectsView, GetProjectById, GetPublicProjectsByGroupId, InactiveProjectsView, ProjectCreateView, ProjectUpdateView, UpdateProjectStatusView, UpdateProjectScoreView, DeleteProjectView
 from .controllers.ProjectBoardController import GetProjectBoards, CreateProjectBoard, GetProjectBoardById, GetVersionProjectBoards, UpdateBoard, DeleteProjectBoard
 from .controllers.TemplateController import GetTemplate, GetAllTemplate, GetTemplateByTeacherId, GetAllPublicTemplates, CreateTemplate, UpdateTemplate, DeleteTemplate
 from .controllers.StudentController import RegisterStudent, LoginStudent, LogoutStudent, StudentView, UpdateGroupFK
 from .controllers.TeacherController import RegisterTeacher, LoginTeacher, LogoutTeacher, TeacherView, GetTeacherById
-from .controllers.GroupController import CreateGroup, GetGroupById, GetGroupsAndProjects, GetGroupByClassId, UpdateGroup
+from .controllers.GroupController import CreateGroup, GetGroupById, GetGroupByClassId, UpdateGroup
 from .controllers.AdminController import RegisterAdmin, LoginAdmin, AdminView, LogoutAdmin
 
 urlpatterns = [
@@ -26,22 +26,13 @@ urlpatterns = [
 
     path('api/teacher/<int:id>', GetTeacherById.as_view()),
 
-
+    path('api/group/group_proj', GetGroupsAndProjects.as_view()),
     path('api/group/<int:group_id>/projects', GetProjectsByGroupId.as_view()),
-    path('api/group/<int:group_id>/public-projects',
-         GetPublicProjectsByGroupId.as_view()),
     path('api/project', ProjectView.as_view()),
     path('api/project/<int:project_id>', GetProjectById.as_view()),
-    path('api/project/public', GetActiveProjectsView.as_view()),
     path('api/project/create', ProjectCreateView.as_view()),
     path('api/project/<int:project_id>/update', ProjectUpdateView.as_view()),
-    path('api/project/<int:project_id>/update_score',
-         UpdateProjectScoreView.as_view()),
-    path('api/project/<int:project_id>/update_status',
-         UpdateProjectStatusView.as_view()),
     path('api/project/<int:project_id>/delete', DeleteProjectView.as_view()),
-
-    path('api/inactive_proj', InactiveProjectsView.as_view()),
 
     path('api/classroom/create', CreateClassroom.as_view()),
     path('api/classroom/all', GetAllClassrooms.as_view()),
@@ -50,8 +41,6 @@ urlpatterns = [
     path('api/classroom/<int:class_id>/group', GetGroupByClassId.as_view()),
     path('api/classroom/<int:class_id>/class_group_proj',
          GetClassroomGroupsAndProjects.as_view()),
-    path('api/classroom/<int:classroom_id>/groupproject',
-         GetTopProjectsByClassroom.as_view()),
 
     path('api/project/<int:project_id>/projectboards', GetProjectBoards.as_view()),
     path('api/projectboards/<int:projectboard_id>',
@@ -66,7 +55,6 @@ urlpatterns = [
 
     path('api/template/<int:template_id>', GetTemplate.as_view()),
     path('api/template/', GetAllTemplate.as_view()),
-    path('api/template/public', GetAllPublicTemplates.as_view()),
     path('api/teacher/template/<int:teacher_id>/',
          GetTemplateByTeacherId.as_view()),
     path('api/template/add', CreateTemplate.as_view()),
@@ -75,7 +63,7 @@ urlpatterns = [
 
     path('api/group/create', CreateGroup.as_view()),
     path('api/group/<int:group_id>', GetGroupById.as_view()),
-    path('api/group/group_proj', GetGroupsAndProjects.as_view()),
+
     path('api/update-group-fk/<int:user_id>', UpdateGroupFK.as_view()),
     path('api/group/<int:group_id>/update', UpdateGroup.as_view()),
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-// const API_HOST = "http://127.0.0.1:8000";
-const API_HOST = "https://bobkyle.pythonanywhere.com";
+const API_HOST = "http://127.0.0.1:8000";
+// const API_HOST = "https://bobkyle.pythonanywhere.com";
 
 export default {
   API_HOST,
