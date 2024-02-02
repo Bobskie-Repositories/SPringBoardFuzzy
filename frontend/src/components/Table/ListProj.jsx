e9148e833119a7029c75d0595a8765282d1948cd
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-02-02 21:10:18 +0800

    updated tables

--- frontend/src/components/Table/ListProj.jsx
+++ frontend/src/components/Table/ListProj.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
-import AdminTable from "./AdminTable";
-import PublicTable from "./PublicTable.jsx";
-import { DateRangePicker } from "rsuite";
-import { FaCaretDown, FaCalendarAlt } from "react-icons/fa";
-import { SiGoogleclassroom } from "react-icons/si";
-import { GrStatusGoodSmall } from "react-icons/gr";
-import global from "../../assets/global.module.css";
-import config from "../../config";
-import axios from "axios";
-import styles from "./Table.module.css";
-import "rsuite/dist/rsuite-no-reset.css";
+import React, { useState, useEffect, useRef } from 'react';
+import AdminTable from './AdminTable';
+import PublicTable from './PublicTable.jsx';
+import { DateRangePicker } from 'rsuite';
+import { FaCaretDown, FaCalendarAlt } from 'react-icons/fa';
+import { SiGoogleclassroom } from 'react-icons/si';
+import { GrStatusGoodSmall } from 'react-icons/gr';
+import global from '../../assets/global.module.css';
+import config from '../../config';
+import axios from 'axios';
+import styles from './Table.module.css';
+import 'rsuite/dist/rsuite-no-reset.css';
 
 const ListProj = ({ admin }) => {
   const [classroom, setClassroom] = useState([]);
@@ -20,20 +20,17 @@ const ListProj = ({ admin }) => {
   const [statusVisible, setStatusVisible] = useState(false);
   const [selectedDates, setSelectedDates] = useState([]);
   const [isActive, setIsActive] = useState(
-    localStorage.getItem("selectedStatus") === "Active Projects" ||
-      localStorage.getItem("selectedStatus") === null
+    localStorage.getItem('selectedStatus') === 'Active Projects' ||
+      localStorage.getItem('selectedStatus') === null
   );
 
   const { API_HOST } = config;
 
   useEffect(() => {
-    localStorage.setItem(
-      "selectedStatus",
-      isActive ? "Active Projects" : "Inactive Projects"
-    );
+    localStorage.setItem('selectedStatus', isActive ? 'Active Projects' : 'Inactive Projects');
 
     const currentPath = window.location.pathname;
-    sessionStorage.setItem("dashboard", currentPath);
+    sessionStorage.setItem('dashboard', currentPath);
   }, [isActive]);
 
   useEffect(() => {
@@ -49,7 +46,7 @@ const ListProj = ({ admin }) => {
 
   const handleStatusChange = (e) => {
     const selectedStatus = e.target.value;
-    setIsActive(selectedStatus === "Active Projects");
+    setIsActive(selectedStatus === 'Active Projects');
     setStatusVisible(false); // Close status dropdown when a status is selected
   };
 
@@ -70,9 +67,9 @@ const ListProj = ({ admin }) => {
   };
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
 
@@ -98,16 +95,13 @@ const ListProj = ({ admin }) => {
   return (
     <div>
       {admin ? (
-        <div style={{ marginTop: "50px" }}>
+        <div style={{ marginTop: '50px' }}>
           <div className={styles.otherHead}>
             <div className={styles.top}>
               <div className={styles.dropdown} ref={dropdownRef}>
                 <div className={styles.icons}>
                   <div className={styles.calendar}>
-                    <FaCalendarAlt
-                      size={18}
-                      style={{ verticalAlign: "middle" }}
-                    />
+                    <FaCalendarAlt size={18} style={{ verticalAlign: 'middle' }} />
                     &nbsp;&nbsp;&nbsp;
                     <DateRangePicker
                       appearance="subtle"
@@ -121,35 +115,26 @@ const ListProj = ({ admin }) => {
                   </div>
 
                   <div className={styles.dropbtn} onClick={handleStatusClick}>
-                    <GrStatusGoodSmall
-                      size={12}
-                      style={{ verticalAlign: "middle" }}
-                    />
+                    <GrStatusGoodSmall size={12} style={{ verticalAlign: 'middle' }} />
                     &nbsp;&nbsp;&nbsp;
                     <p className={styles.classroomText}>
-                      {isActive ? "Active Projects" : "Inactive Projects"}
+                      {isActive ? 'Active Projects' : 'Inactive Projects'}
                     </p>
                     &nbsp;&nbsp;&nbsp;
-                    <FaCaretDown style={{ verticalAlign: "middle" }} />
+                    <FaCaretDown style={{ verticalAlign: 'middle' }} />
                   </div>
 
                   <div className={styles.dropbtn} onClick={handleDropdownClick}>
-                    <SiGoogleclassroom
-                      size={20}
-                      style={{ verticalAlign: "middle" }}
-                    />
+                    <SiGoogleclassroom size={20} style={{ verticalAlign: 'middle' }} />
                     &nbsp;&nbsp;&nbsp;
                     <p className={styles.classroomText}>All Classrooms</p>
                     &nbsp;&nbsp;&nbsp;
-                    <FaCaretDown style={{ verticalAlign: "middle" }} />
+                    <FaCaretDown style={{ verticalAlign: 'middle' }} />
                   </div>
                 </div>
 
                 {dropdownVisible && (
-                  <div
-                    className={styles.dropdowncontent}
-                    style={{ minWidth: "12rem" }}
-                  >
+                  <div className={styles.dropdowncontent} style={{ minWidth: '12rem' }}>
                     {classroom.map((clsrm) => (
                       <div key={clsrm.id} className={styles.classroomItem}>
                         <input
@@ -160,7 +145,7 @@ const ListProj = ({ admin }) => {
                           className={styles.box}
                         />
                         <label
-                          style={{ color: "#9c7b16", margin: "0 10px 0 0" }}
+                          style={{ color: '#9c7b16', margin: '0 10px 0 0' }}
                           htmlFor={`classroom-${clsrm.id}`}
                         >
                           {clsrm.class_name}
@@ -177,7 +162,7 @@ const ListProj = ({ admin }) => {
                         className={`${global.center} ${styles.customLi}`}
                         onClick={() =>
                           handleStatusChange({
-                            target: { value: "Active Projects" },
+                            target: { value: 'Active Projects' },
                           })
                         }
                       >
@@ -187,7 +172,7 @@ const ListProj = ({ admin }) => {
                         className={`${global.center} ${styles.customLi}`}
                         onClick={() =>
                           handleStatusChange({
-                            target: { value: "Inactive Projects" },
+                            target: { value: 'Inactive Projects' },
                           })
                         }
                       >
@@ -214,17 +199,13 @@ const ListProj = ({ admin }) => {
               id="status"
               className={styles.textInput_select}
               onChange={handleStatusChange}
-              value={isActive ? "Active Projects" : "Inactive Projects"}
+              value={isActive ? 'Active Projects' : 'Inactive Projects'}
             >
               <option>Active Projects</option>
               <option>Inactive Projects</option>
             </select>
           </div>
-          <PublicTable
-            isActive={isActive}
-            filter={selectedClassrooms}
-            allProjects={allProjects}
-          />
+          <PublicTable isActive={isActive} filter={selectedClassrooms} allProjects={allProjects} />
         </>
       )}
     </div>
